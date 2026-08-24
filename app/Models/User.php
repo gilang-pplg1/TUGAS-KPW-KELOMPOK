1a1d72ffa45b417a3a497b21fc06a18dbb4785c4
gilang-pplg1 <gilangramburabbani575@gmail.com>
2026-08-24 18:31:30 +0700

    shaquil peran dan user

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,13 +13,13 @@ class User extends Authenticatable
     protected $table = 'user';
     protected $fillable = ['name', 'email', 'password'];
 
-    // Relasi One to One ke Profile
+        // Relasi One to One ke Profile // Relasi One to One ke Profile
     public function profile()
     {
         return $this->hasOne(Profile::class, 'user_id');
     }
 
-    // Relasi One to Many ke Kritik
+    // Relasi One to Many ke Kritik  // Relasi One to Many ke Kritik
     public function kritik()
     {
         return $this->hasMany(Kritik::class, 'user_id');
